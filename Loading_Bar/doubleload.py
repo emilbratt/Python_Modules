8774bad4416aac7f84ec701ef37fde480ebc05d6
emilbratt <emilbratt@gmail.com>
2020-11-28 23:25:28 +0100

    removing unnecessary lines of code that I oversaw

--- Loading_Bar/doubleload.py
+++ Loading_Bar/doubleload.py
@@ -25,15 +25,6 @@ class Loadbar:
         self.clearScreen = lambda : os.system(
             'cls' if os.name == 'nt' else 'clear')
 
-        # base 1, 10 or 100 affects the increments
-        # for number of digits in each round
-        if len(str(laps)) < 2:
-            self.base = 1
-        if len(str(laps)) == 2:
-            self.base = 10
-        if len(str(laps)) > 2:
-            self.base = 100
-
         self.laps = laps
         self.taskname = taskname[:51]
         self.horizontal = '+'+'-'*51 +'+'
@@ -42,15 +33,9 @@ class Loadbar:
 
 
     def display(self, round):
-
         round += 1
 
-         # force increments within 100% regardless if laps is  < 100
-        basePercent = int(str(
-            self.base).ljust(3, '0'))
-
-        # calculate progress in %
-        progress = (basePercent/self.laps)*round
+        progress = (100/self.laps)*round
         showPercentage = 'Progress: '+str(
             int(progress))+'/100%'
 
@@ -85,7 +70,7 @@ class Loadbar:
         print(f'+{showPercentage.center(51, "-")}+')
         print(f'|{loadingLeft}|')
         print(self.horizontal)
-        
+
         if round != self.laps:
             if self.platform.startswith(
             'linux'
